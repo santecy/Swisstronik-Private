879d76379581110cfa796a47072576d105f97f52
santecy <santecy.nft@gmail.com>
2024-07-29 17:17:04 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x913fc4335E4E549BC1B3b34CF5c6729Bbc578acA'
+export const ProxyAdmin = '0x9A834aCF7Ca09713C17aCedB98Ce468F75dc9181'
+export const SWTRImplementation = '0x6f119c25a78b5Ee29Ef23ebbEB2c776683d5B7f9'
     
